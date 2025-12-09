68956fddd75d99ba5e338d1f811a8a0051c9e1be
kabil-AK <kabilanrajini007@gmail.com>
2025-12-09 05:39:13 -0500

    up file

--- config/db.js
+++ config/db.js
@@ -6,8 +6,10 @@ const connectDB = async () => {
     await mongoose.connect(uri);
     console.log('MongoDB connected');
   } catch (err) {
-    console.error('MongoDB connection error', err.message);
-    process.exit(1);
+    console.error('MongoDB connection error:', err.message);
+
+    
+    setTimeout(connectDB, 5000);
   }
 };
 
